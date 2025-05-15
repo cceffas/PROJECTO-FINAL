3e9d81b2829f06cabca627e981a7e223029aa0a8
devcefas <devcefas@gmail.com>
2025-05-15 21:13:24 +0100

    gerar notas e media bug corrugido

--- resources/views/forms/desempenhoTurma.blade.php
+++ resources/views/forms/desempenhoTurma.blade.php
@@ -2,13 +2,13 @@
 
 @section('content')
     <div class="mt-20 space-y-10">
-
         <x-Title-app title='desempenho > turma > {{ $turma->nome }}' action='/desempenho' text_action='voltar'
             type='secondary' />
 
         <x-bladewind::card>
             <form action="/desempenho/criar" method="POST">
                 @csrf
+
                 <x-bladewind::table>
                     <x-slot name='header'>
                         <tr>
@@ -25,46 +25,30 @@
                     </x-slot>
 
                     @foreach ($turma->alunos()->get() as $aluno)
+                        @php
+                            $notas = $aluno->notas()->take(3)->get(); // pega as 3 notas
+                            $somaNotas = $notas->sum('valor');
+                            $media = $notas->count() > 0 ? $somaNotas / $notas->count() : 0;
+                        @endphp
+
                         <tr>
                             <td>{{ $aluno->id }}</td>
                             <td>{{ $aluno->nome }}</td>
 
-                            @php
-                                $notas = $aluno->notas()->get();
-                                $somaNotas = $notas->sum('valor');
-                                $media = $notas->count() > 0 ? $somaNotas / $notas->count() : 0;
-                            @endphp
-
-                            @foreach ($notas as $index => $nota)
+                            @foreach ($notas as $nota)
                                 <td>
                                     <input
                                         type="number"
                                         step="0.01"
                                         min="0"
                                         max="20"
-                                        name="notas[{{ $aluno->id }}][{{ $nota->id }}]"
+                                        name="notas[{{ $nota->id }}]"
                                         value="{{ $nota->valor }}"
                                         style="width: 50px; font-size: 13px; border: none;"
                                         class="rounded text-blue-500 cursor-pointer"
                                     />
                                 </td>
                             @endforeach
-
-                            {{-- Preenche as colunas vazias se tiver menos de 3 notas --}}
-                            @for ($i = $notas->count(); $i < 3; $i++)
-                                <td>
-                                    <input
-                                        type="number"
-                                        step="0.01"
-                                        min="0"
-                                        max="20"
-                                        name="notas[{{ $aluno->id }}][nova_{{ $i }}]"
-                                        value=""
-                                        style="width: 50px; font-size: 13px; border: none;"
-                                        class="rounded text-blue-500 cursor-pointer"
-                                    />
-                                </td>
-                            @endfor
 
                             <td>{{ number_format($media, 1) }}</td>
                             <td></td>
@@ -74,6 +58,5 @@
                 </x-bladewind::table>
             </form>
         </x-bladewind::card>
-
     </div>
 @endsection
